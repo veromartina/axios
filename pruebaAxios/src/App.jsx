a97b45fb75acd8fc331f0b48e6735dec17b7e8bf
Veronica <veromartina8@gmail.com>
2025-01-04 01:06:37 -0300

    se agrega funcionalidad a los componentes

--- pruebaAxios/src/App.jsx
+++ pruebaAxios/src/App.jsx
@@ -1,16 +1,37 @@
-//import { List } from '@chakra-ui/react'
-import { Header } from './components/Header'
-import { Card } from './components/Card'
-import { List } from './components/List'
+import { VStack } from "@chakra-ui/react";
+import List from "./components/List";
+import Header from "./components/Header"
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  
+  const [data, setData] = useState([]);
+  const [gender, setGender] = useState("");
+  const [name, setName] = useState("");
+  const [status, setStatus] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character?gender=${gender}&name=${name}&status=${status}`)
+      .then(({ data }) => {
+        setData(data.results);
+      });
+  }, [gender]);
+
   return (
-  <Header></Header>
-    <List/>
-    <Card/>
-   
-  )
+    <VStack>
+      <Header
+       gender={gender}
+       setGender={setGender}
+       setName={setName}
+       status={status}
+       setStatus={setStatus}
+       name={name}/>
+
+      <List
+        data={data}/>
+    </VStack>
+  );
 }
 
-export default App
+export default App;
--- pruebaAxios/src/components/Header.jsx
+++ pruebaAxios/src/components/Header.jsx
@@ -1,11 +1,56 @@
-import { Heading, VStack} from '@chakra-ui/react'
+import {
+  Heading,
+  VStack,
+  Input,
+  InputRightElement,
+  Button,
+  InputGroup,
+  Select,
+} from "@chakra-ui/react";
 
+const Header = (name, setName, setGender, setStatus, Gender, status) => {
+  const handleClick = () => setName(name);
 
-const Header = ()=>{
-    return (
-        <VStack>
-            <Heading>Lista de Personajes </Heading>
-        </VStack>
-    )
-}
-export default Header
+  return (
+    <VStack>
+      <Heading>Lista de Personajes </Heading>
+      <InputGroup size="md">
+        <Input
+          pr="4.5rem"
+          type={"text"}
+          placeholder="ingrese algo"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <InputRightElement width="4.5rem">
+          <Button h="1.75rem" size="sm" onClick={handleClick}>
+            OK
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+      <Select
+        placeholder="Select option"
+        onChange={(e) => {setGender(e.target.value); }}
+        value={Gender}
+      >
+        <option value="">all</option>
+        <option value="Female">Female</option>
+        <option value="Male">Male</option>
+        <option value="Genderless">Genderless</option>
+        <option value="unknown">Unknown</option>
+      </Select>
+
+      <Select
+        placeholder="Select option"
+        onChange={(e) => {setStatus(e.target.value) }}
+        value={status}
+      >
+        <option value="">All</option>
+        <option value="Alive">Alive</option>
+        <option value="Dead">Dead</option>
+        <option value="Unknown">Unknown</option>
+      </Select>
+    </VStack>
+  );
+};
+export default Header;
